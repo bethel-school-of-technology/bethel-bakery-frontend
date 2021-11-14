cd4804bf8ef26ffd1cbdcacedfb0172feae9b428
cclark815 <82556630+cclark815@users.noreply.github.com>
2021-11-14 00:24:52 -0600

    starting routing

--- bethel-bakery-frontend/src/app/app.module.ts
+++ bethel-bakery-frontend/src/app/app.module.ts
@@ -3,28 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AssortedPastryTrayComponent } from './assorted-pastry-tray/assorted-pastry-tray.component';
-import { ChocolateCaramelPetitComponent } from './chocolate-caramel-petit/chocolate-caramel-petit.component';
-import { ChocolateMintChipComponent } from './chocolate-mint-chip/chocolate-mint-chip.component';
-import { ChritmasCookiesComponent } from './chritmas-cookies/chritmas-cookies.component';
-import { DutchBrownieComponent } from './dutch-brownie/dutch-brownie.component';
-import { GrandmasChocolateChipComponent } from './grandmas-chocolate-chip/grandmas-chocolate-chip.component';
-import { CoveredAlmondsComponent } from './covered-almonds/covered-almonds.component';
-import { RedVelvetPopsComponent } from './red-velvet-pops/red-velvet-pops.component';
-import { VanillaCakePopsComponent } from './vanilla-cake-pops/vanilla-cake-pops.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AssortedPastryTrayComponent,
-    ChocolateCaramelPetitComponent,
-    ChocolateMintChipComponent,
-    ChritmasCookiesComponent,
-    DutchBrownieComponent,
-    GrandmasChocolateChipComponent,
-    CoveredAlmondsComponent,
-    RedVelvetPopsComponent,
-    VanillaCakePopsComponent
+
   ],
   imports: [
     BrowserModule,
