7ade4ca8f467ca9d8dd3a5e6031675a224baddac
Rickey-McCalop <rickeymccalop@outlook.com>
2021-11-28 17:15:41 -0600

    Login html

--- bethel-bakery-frontend/src/app/components/customer-details/customer-details.component.ts
+++ bethel-bakery-frontend/src/app/components/customer-details/customer-details.component.ts
@@ -1,7 +1,11 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { url } from 'inspector';
 import { Customer } from 'src/app/models/customer/customer';
 import { CartDataService } from 'src/app/services/cart-data/cart-data.service';
+import { CartDetailsComponent } from '../cart-details/cart-details.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-customer-details',
@@ -11,27 +15,31 @@ import { CartDataService } from 'src/app/services/cart-data/cart-data.service';
 export class CustomerDetailsComponent implements OnInit {
 
   newCustomer: Customer = new Customer();
+  // newDetail: Detail = new Detail();
+  // private _url: string = '';
 
-
-  constructor(private router: Router, private newCustomerInfo: CartDataService) {}
+  constructor(private router: Router, private newCustomerInfo: CartDataService, private http: HttpClient) { }
 
 
   ngOnInit(): void {
+    // this.getInfo();
   }
 
-  // let firstName;
-
-  // firstName=document.getElementById("firstName").value;
-
-  // localStorage.setItem("firstName", firstName)
-  createNew(){
-    this.newCustomerInfo.createCustomer(this.newCustomer).subscribe(response =>{
+  createNew() {
+    this.newCustomerInfo.createCustomer(this.newCustomer).subscribe(response => {
       console.log(response);
-    })
+    });
+    // tslint:disable-next-line: typedef
+    // getInfo = () => {
+    //   this.httpClient.get();
+    // };
+
   }
 
-  btnClick =  () => {
-    this.router.navigateByUrl('/confirmation.component.html');
-    // subscribe?
-  }
+
+  //  return this.router.navigateByUrl('/confirmation.component.html');
+
+
 }
+
+
