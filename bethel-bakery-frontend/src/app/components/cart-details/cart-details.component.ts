87fe32a1ee87c937be70f73a0800a0b046148759
Brian DeWitt <enigmabd1704@yahoo.com>
2021-11-28 12:24:56 -0800

    Cart-Details with local storage Initial Commit

--- bethel-bakery-frontend/src/app/components/cart-details/cart-details.component.ts
+++ bethel-bakery-frontend/src/app/components/cart-details/cart-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CartItem } from 'src/app/models/cart-item/cart-item';
 
 @Component({
   selector: 'app-cart-details',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartDetailsComponent implements OnInit {
 
+newCartItemOne: CartItem = new CartItem();
+ 
+
   constructor() { }
+  
 
   ngOnInit(): void {
   }
 
+  getCartItem(newCartItemOne){
+    this.newCartItemOne = JSON.parse(localStorage.getItem("key"));
+    return newCartItemOne;
+  }
 }
