c2f07692f179e57f8911484be696a8dfe874a79f
Rickey-McCalop <rickeymccalop@outlook.com>
2021-12-01 23:58:30 -0600

    Login Service Added

---
+++ bethel-bakery-frontend/src/app/services/login.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Customer } from 'src/app/models/customer/customer';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  constructor(privatr http: HTTPClient) { }
+}
