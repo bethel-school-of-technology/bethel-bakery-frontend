b493f06ab5a5770216f1859fdbd36829fd6f788d
Brian DeWitt <enigmabd1704@yahoo.com>
2021-12-03 18:31:54 -0800

    delete button/keyArray/whileloop

--- bethel-bakery-frontend/src/app/components/cart-details/cart-details.component.ts
+++ bethel-bakery-frontend/src/app/components/cart-details/cart-details.component.ts
@@ -9,9 +9,27 @@ import { CartItem } from 'src/app/models/cart-item/cart-item';
 export class CartDetailsComponent implements OnInit {
 
 newCartItemOne: CartItem = new CartItem();
+//I am working on an array complete with if statement and loop to check contents of local storage and load all objects. Due to the way localstorage works with keys...
+//It appears that all keys will need to be saved into variables, and stored into an array, and then I can run a loop on the array to load the whole cart.
+keyArray:string[] = [];
+x = "";
  
 
+  
   constructor() { }
+//Working on a loop to provide the key values for the local storage...
+  //Getting an error about using type keyArray as an injectable...will continue working on it.
+
+   /*constructor(keyArray: { value: any; index: number; }, x: any) {
+    while (keyArray != null){
+      x = keyArray.value;
+      keyArray.index ++;
+      return x;
+    }
+    if (keyArray = null){
+      console.log("keyArray: Empty");
+    }
+   }*/
   
 
   //This function should be working if the database is running and the "key" is updated. It will generate null otherwise.
@@ -23,7 +41,15 @@ newCartItemOne: CartItem = new CartItem();
 
   //Set key to the actual key in order for this to !null. newCartItemOne should now have JSON properties.
   getCartItem(newCartItemOne): CartItem {
-    this.newCartItemOne = JSON.parse(localStorage.getItem("key"));
+    this.newCartItemOne = JSON.parse(localStorage.getItem(this.keyArray.values.toString())); //Really not sure about this, might revert to keyArray index, or otherwise.
     return newCartItemOne;
   }
+
+
+/*deleteCartItemClick(newCartItemOne): CartItem {
+document.getElementById("deleteCartItemOne").onclick(deleteCartItemFromLocalStorage())
+}
+deleteCartItemFromLocalStorage(){
+  this.keyArray.reduce();
+}*/
 }
