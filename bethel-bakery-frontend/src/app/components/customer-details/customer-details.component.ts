ab2c596cb763309c12ba0d2946266e523fa91bdc
Rickey-McCalop <rickeymccalop@outlook.com>
2021-12-05 14:47:14 -0600

    Uncommented code

--- bethel-bakery-frontend/src/app/components/customer-details/customer-details.component.ts
+++ bethel-bakery-frontend/src/app/components/customer-details/customer-details.component.ts
@@ -1,60 +1,60 @@
-// import { HttpClient } from '@angular/common/http';
-// import { Component, OnInit } from '@angular/core';
-// import { Router } from '@angular/router';
-// import { url } from 'inspector';
-// import { Customer } from 'src/app/models/customer/customer';
-// import { CartDataService } from 'src/app/services/cart-data/cart-data.service';
-// import { CartDetailsComponent } from '../cart-details/cart-details.component';
-// import { HttpClientModule } from '@angular/common/http';
-// import { Product } from 'src/app/models/product/product';
-// import { CartItem } from 'src/app/models/cart-item/cart-item';
-// import { OrderDetailsService } from 'src/app/services/order-details.service';
-// @Component({
-//   selector: 'app-customer-details',
-//   templateUrl: './customer-details.component.html',
-//   styleUrls: ['./customer-details.component.css']
-// })
-// export class CustomerDetailsComponent implements OnInit {
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { url } from 'inspector';
+import { Customer } from 'src/app/models/customer/customer';
+import { CartDataService } from 'src/app/services/cart-data/cart-data.service';
+import { CartDetailsComponent } from '../cart-details/cart-details.component';
+import { HttpClientModule } from '@angular/common/http';
+import { Product } from 'src/app/models/product/product';
+import { CartItem } from 'src/app/models/cart-item/cart-item';
+import { OrderDetailsService } from 'src/app/services/order-details.service';
+@Component({
+  selector: 'app-customer-details',
+  templateUrl: './customer-details.component.html',
+  styleUrls: ['./customer-details.component.css']
+})
+export class CustomerDetailsComponent implements OnInit {
 
-//   newOrder: Order = new Order();
+  newOrder: Order = new Order();
   
 
-//   id: number = 0;
-//   firstName: string = "";
-//   lastName: string = "";
-//   address: string = "";
-//   email: string = "";
-//   phoneNumber: string = "";3
-//   creditCardNumber: string = "";
-//   expiration: string = "";
-//   ccv: string = "";
-//   subTotal: number = 0 ;
-//   total: number = 0 ;
-//   dateTimeStamp: Date ; 
-//   cartItems: CartItem[] = [];
-//   customer: Customer = new Customer; 
-//  static orderInfo: any;
+  id: number = 0;
+  firstName: string = "";
+  lastName: string = "";
+  address: string = "";
+  email: string = "";
+  phoneNumber: string = "";3
+  creditCardNumber: string = "";
+  expiration: string = "";
+  ccv: string = "";
+  subTotal: number = 0 ;
+  total: number = 0 ;
+  dateTimeStamp: Date ; 
+  cartItems: CartItem[] = [];
+  customer: Customer = new Customer; 
+ static orderInfo: any;
 
 
-//   constructor(private router: Router, private newCustomerInfo: CartDataService, private http: HttpClient, private orderDetailsService: OrderDetailsService) { }
+  constructor(private router: Router, private newCustomerInfo: CartDataService, private http: HttpClient, private orderDetailsService: OrderDetailsService) { }
 
 
-//   ngOnInit(): void {
-//   }
-//   getItems(product: Product): void {
+  ngOnInit(): void {
+  }
+  getItems(product: Product): void {
    
-//     let url = 'loalhost:8080/order';
-//   //  what is the URL for posting data to the Db?
-//     if (localStorage.getItem('cartItems') != null) {
-//       this.cartItems = JSON.parse(localStorage.getItem('cartItems'));
-//      }
-//     }
-//    addOrder = (order:Order) => {
-//      this.orderDetailsService.newOrder(Order).subscribe(response => {
-//         // adding to the Db
-//      } )
-//    }
-//   }
+    let url = 'loalhost:8080/order';
+  //  what is the URL for posting data to the Db?
+    if (localStorage.getItem('cartItems') != null) {
+      this.cartItems = JSON.parse(localStorage.getItem('cartItems'));
+     }
+    }
+   addOrder = (order:Order) => {
+     this.orderDetailsService.newOrder(Order).subscribe(response => {
+        // adding to the Db
+     } )
+   }
+  }
   
 
 
--- bethel-bakery-frontend/src/app/components/login/login.component.ts
+++ bethel-bakery-frontend/src/app/components/login/login.component.ts
@@ -1,38 +1,38 @@
-// import { Component, OnInit } from '@angular/core';
-// import { CartItem } from 'src/app/models/cart-item/cart-item';
-// import { Customer } from 'src/app/models/customer/customer';
-// import { CartDataService } from 'src/app/services/cart-data/cart-data.service';
-// @Component({
-//   selector: 'app-login',
-//   templateUrl: './login.component.html',
-//   styleUrls: ['./login.component.css']
-// })
-// export class LoginComponent implements OnInit {
-//   newCustomer: Customer = new Customer();
+import { Component, OnInit } from '@angular/core';
+import { CartItem } from 'src/app/models/cart-item/cart-item';
+import { Customer } from 'src/app/models/customer/customer';
+import { CartDataService } from 'src/app/services/cart-data/cart-data.service';
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  newCustomer: Customer = new Customer();
 
-//   id: number = 0;
-//   userName: string = '';
-//   password: string = '';
-//   isAdmin: boolean;
-//   cartItems: CartItem ;
-//   orders: CartItem ;
+  id: number = 0;
+  userName: string = '';
+  password: string = '';
+  isAdmin: boolean;
+  cartItems: CartItem ;
+  orders: CartItem ;
 
-//   constructor(private newCustomerInfo: CartDataService) { }
-//   // will we need to access cardDataService in order to refference current users order history or just declare/import order history? is cartDataService soley for localStorage for active orders?
+  constructor(private newCustomerInfo: CartDataService) { }
+  // will we need to access cardDataService in order to refference current users order history or just declare/import order history? is cartDataService soley for localStorage for active orders?
 
-//   ngOnInit(): void {
-//   }
-//   createNew() {
-//     this.newCustomerInfo.createCustomer(this.newCustomer).subscribe(response => {
-//       console.log(response);
-//     });
-//   getUser(){
+  ngOnInit(): void {
+  }
+  createNew() {
+    this.newCustomerInfo.createCustomer(this.newCustomer).subscribe(response => {
+      console.log(response);
+    });
+  getUser(){
     
-//   }
-// //  getUser() = click event to get user from Db
-//   }
+  }
+//  getUser() = click event to get user from Db
+  }
   
 
-// }
+}
 
-// // comment out code until Customer model is change after order model is merged and CustDet is uncommented 
+// comment out code until Customer model is change after order model is merged and CustDet is uncommented 
