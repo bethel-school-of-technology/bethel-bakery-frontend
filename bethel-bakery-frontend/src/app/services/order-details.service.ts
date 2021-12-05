4884e89a7a46f96d82e2ed45de447ecdcd71302d
Rickey-McCalop <rickeymccalop@outlook.com>
2021-12-04 22:45:17 -0600

    update 1

---
+++ bethel-bakery-frontend/src/app/services/order-details.service.ts
@@ -0,0 +1,15 @@
+// import { Injectable } from '@angular/core';
+// import { HttpClient } from '@angular/common/http';
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class OrderDetailsService {
+
+//   constructor(private http:HttpClient) {
+
+//    }
+//   newOrder(order: Order):Obsevable<Order>{
+//      return this.http.post<Order>('loalhost:8080/order', order);
+//    }
+// }
