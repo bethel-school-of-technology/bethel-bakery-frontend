c64735fb165347210d10c78be4f5b88c483cc334
Brian DeWitt <enigmabd1704@yahoo.com>
2021-12-06 20:25:45 -0800

    LocalStorange branch manually added to main

--- bethel-bakery-frontend/src/app/models/product/product.ts
+++ bethel-bakery-frontend/src/app/models/product/product.ts
@@ -1,6 +1,5 @@
 export class Product {
-    productId: number = 0;
+    id: number = 0;
     name: string = "";
     price: number = 0;
-    imgUrl: string = "";
-}
+    imgUrl: string = "";}
--- bethel-bakery-frontend/src/app/services/cart-data/cart-data.service.ts
+++ bethel-bakery-frontend/src/app/services/cart-data/cart-data.service.ts
@@ -3,50 +3,73 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { CartItem } from 'src/app/models/cart-item/cart-item';
 import { Customer } from 'src/app/models/customer/customer';
+import { Product } from 'src/app/models/product/product';
+import { MoneyService } from '../money/money.service';
 
-
-
-
-const httpOptions = 
-{
+ 
+const httpOptions = {
+  
   headers: new HttpHeaders({'Content-type': 'application/json'})
 }
-
 @Injectable({
   providedIn: 'root'
 })
+
+//Service for managing the data of cartItems
 export class CartDataService {
-
-  addToCartUrl: string = "http://localhost:8080/cart/add"
-  cartUrl: string = "http://localhost:8080/cart"
-
   cartItems: CartItem[] = [];
+  cartUrl = "";
+  addToCartUrl = "";
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private moneyService: MoneyService) { }
+
+  addCartItemToLocalStorage(product: Product): void {
+
+    //checks to see if there is any items in cartItems
+    if(localStorage.getItem("cartItems") != null){
+      this.cartItems = JSON.parse(localStorage.getItem("cartItems"));
+    }
+
+    //Checks to see if product already exist in cart
+    let foundProduct: Boolean = false;
+    let productIndex: number = 0;
+    for(let i: number = 0; i < this.cartItems.length; i++){
+      if(this.cartItems[i].product.id === product.id){
+        foundProduct = true;
+        productIndex = i;
+      }
+    }
+
+    //if there product does exist in array than increase quantity
+    if(foundProduct == true){
+      this.cartItems[productIndex].productQuantity++;
+      this.cartItems[productIndex].subTotal = this.moneyService.calculateSubTotal     (this.cartItems[productIndex]);
+
+    }
+    //if product does not exist in array than create a new cart item.
+    else {
+      let newCartItem: CartItem = new CartItem();
+      newCartItem.product = product;
+      newCartItem.productQuantity = 1;
+      newCartItem.subTotal = product.price;
+      this.cartItems.push(newCartItem);
+    }
+
+    //sore to local storage
+    localStorage.setItem("cartItems", JSON.stringify(this.cartItems));
+
+    //debug
+    this.cartItems = JSON.parse(localStorage.getItem("cartItems"));
+    console.log("New Set:")
+    console.log(this.cartItems);
+    console.log("");
+  }
 
   saveProduct (productId: number): Observable<any> {
     return this.http.post<any>(this.addToCartUrl + "/" + productId + "/1", productId, httpOptions);
-  }
-
-  getCartItems ():Observable<CartItem[]> 
-  {
-    return this.http.get<CartItem[]>(this.cartUrl);
   }
 
   createCustomer(newCustomer: Customer) : Observable<Customer>{
     return this.http.post<Customer>(this.cartUrl, newCustomer)
   }
 }
-
-  // getTotalInCart = () :number => 
-  // {
-  //   this.getCartItems().subscribe(response => this.cartItems = response);
-  //   let totalQty:number = 0;
-  //   for(var i = 0; i < this.cartItems.length; i++) {
-  //     totalQty = totalQty + this.cartItems[i].productQuantity;
-      
-  //   }
-  //   console.log(totalQty);
-  //   return totalQty;
-  // } 
-
