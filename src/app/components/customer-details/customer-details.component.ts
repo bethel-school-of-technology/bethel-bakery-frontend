210485547ef8f909027657f5b329a4294b3ca30a
Rickey-McCalop <rickeymccalop@outlook.com>
2021-12-10 23:30:41 -0600

    Nothing

--- src/app/components/customer-details/customer-details.component.ts
+++ src/app/components/customer-details/customer-details.component.ts
@@ -19,26 +19,7 @@ export class CustomerDetailsComponent implements OnInit {
   newOrder: Order = new Order();
   url = 'loalhost:8080/order';
 
-<<<<<<< HEAD
-  //   id: number = 0;
-  //   firstName: string = "";
-  //   lastName: string = "";
-  //   address: string = "";
-  //   email: string = "";
-  //   phoneNumber: string = "";3
-  //   creditCardNumber: string = "";
-  //   expiration: string = "";
-  //   ccv: string = "";
-  //   subTotal: number = 0 ;
-  //   total: number = 0 ;
-  //   dateTimeStamp: number = Date.now(); 
-  //   cartItems: CartItem[] = [];
-  //   customer: Customer = new Customer; 
-  //  static orderInfo: any;
-
-=======
   constructor(private router: Router, private cartDataService: CartDataService, private http: HttpClient, private orderDetailsService: OrderDetailsService) { }
->>>>>>> 89fe4c62472c26733f911058592fe19a8ce49835
 
 
   ngOnInit(): void {}
@@ -46,54 +27,12 @@ export class CustomerDetailsComponent implements OnInit {
   submitOrder() {
     this.orderDetailsService.submitOrder(this.newOrder);  
   }
-<<<<<<< HEAD
-  getItems(product: Product): void {
-
-    // let url = 'loalhost:8080/order';
-
-    if (localStorage.getItem('cartItems') != null) {
-      this.cartItems = JSON.parse(localStorage.getItem('cartItems'));
-    }
-  }
-  addOrder() {
-  
-    return this.http.post(this.url, this.newOrder).toPromise().then(data: any =>{
-
-    });
-  }
-
-}
-
-
-
-=======
 
   
 
-  // getItems(product: Product): void {
-   
-  // //  what is the URL for posting data to the Db?
-  //   if (localStorage.getItem('cartItems') != null) {
-  //     let cartItems: CartItem = JSON.parse(localStorage.getItem('cartItems'));
-  //    }
-  //   }
-  //   addOrder(){
-  //     this.orderDetailsService.newOrder(this.newOrder).subscribe(order =>{
-  //       console.log(order);
-
-  //     })
-  //   }
-
-  //  addOrder = (order:Order) => {
-  //    this.orderDetailsService.newOrder(Order).subscribe(response => {
-      
-  //     this
-
-  //       // adding to the Db
-  //    } )
+  
    }
   
   
->>>>>>> 89fe4c62472c26733f911058592fe19a8ce49835
 
 
